eb6fa733e88f184a17b3173e805f2baa76561f63
Benjamin Bordne <benjibordne@outlook.com>
2022-08-10 16:25:06 +0000

    removed extra connection

--- conn-example.php
+++ conn-example.php
@@ -13,12 +13,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// second connection to same database
-$conn2 = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn2->connect_error) {
-    die("Connection failed: " . $conn2->connect_error);
-}
-
 ?>
